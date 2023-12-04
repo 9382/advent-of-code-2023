af40ef658b732c63f5180ac506d7664150c49a05
9382 <omnomaidan@gmail.com>
2023-12-04 15:37:40 +0000

    day 4 part 2 python alternative
    for testing

---
+++ 4/code2-alternate.py
@@ -0,0 +1,24 @@
+Input = open("input.txt", "r").read()
+
+import re
+Lines = Input.split("\n")
+Total = 0
+BonusCards = [0]*(len(Lines)+5) #+5 is for safety buffer towards the end
+for line in Lines:
+	if line == "":
+		continue
+	CardN, Winners, Numbers = re.search("^Card +(\d+): ([^|]+) \| (.+)$", line).groups()
+	CardN = int(CardN)-1
+	ExtraCopies = BonusCards[CardN]
+	WinningNumbers = set()
+	Matches = 0
+	for number in re.findall("\d+", Winners):
+		WinningNumbers.add(number)
+	for number in re.findall("\d+", Numbers):
+		if number in WinningNumbers:
+			Matches = Matches + 1
+	for i in range(1, Matches+1): #cringe python ranges, this is why lua is better
+		BonusCards[CardN+i] = BonusCards[CardN+i] + 1+ExtraCopies
+	Total = Total + 1 + ExtraCopies
+
+print("Total:", Total)
