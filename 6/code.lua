e8045bc4972b025852f3df3af429331698e9395d
9382 <omnomaidan@gmail.com>
2023-12-07 11:27:51 +0000

    day 6 part 1

---
+++ 6/code.lua
@@ -0,0 +1,29 @@
+local Input = io.open("input.txt", "r")
+
+local TimeData = Input:read("*l"):sub(11, -1):gmatch("[^ ]+")
+local DistanceData = Input:read("*l"):sub(11, -1):gmatch("[^ ]+")
+local Total = 1
+while true do
+	local Time, Distance = tonumber(TimeData() or ""), tonumber(DistanceData() or "")
+	if not Time then
+		break
+	end
+	local EarliestSuccess, LatestSuccess
+	for i = 0, Time do
+		local ExpectedDistance = (Time-i)*(i)
+		if ExpectedDistance > Distance then
+			EarliestSuccess = i
+			break
+		end
+	end
+	for i = Time, 0, -1 do
+		local ExpectedDistance = (Time-i)*(i)
+		if ExpectedDistance > Distance then
+			LatestSuccess = i
+			break
+		end
+	end
+	Total = Total * (LatestSuccess - EarliestSuccess + 1)
+	print("Earliest", EarliestSuccess, "Latest", LatestSuccess)
+end
+print("Total:", Total)
