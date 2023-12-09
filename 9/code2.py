63e32987640404bd29030a087c53c375c3498565
9382 <omnomaidan@gmail.com>
2023-12-09 07:23:05 +0000

    and a rather simple part 2
    4 lines are different between this file and part 1's file

---
+++ 9/code2.py
@@ -0,0 +1,24 @@
+Input = open("input.txt", "r").read()
+
+def CalculateNextExpectedChange(Sequence):
+	FirstValue = int(Sequence[0])
+	SubSequence = []
+	IsAllZero = True
+	for i in range(len(Sequence)-1):
+		NextValue = int(Sequence[i+1]) - int(Sequence[i])
+		if NextValue != 0:
+			IsAllZero = False
+		SubSequence.append(NextValue)
+	if IsAllZero: # The sequence has no changes (our rate of change is consistent), send up the consistent change
+		return FirstValue
+	else: # The sequence has a predicted rate of change of whatever the subsequence resolves to, so adjust our last value by that amount
+		return FirstValue-CalculateNextExpectedChange(SubSequence)
+
+Total = 0
+for line in Input.split("\n"):
+	if line == "":
+		continue
+	Sequence = line.split(" ")
+	Total = Total + CalculateNextExpectedChange(Sequence)
+
+print("Total:", Total)
