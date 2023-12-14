133f64ad276744c7a4fe8dca5316cc74dc0d5976
9382 <omnomaidan@gmail.com>
2023-12-14 07:45:27 +0000

    day 14 part 1

---
+++ 14/code.lua
@@ -0,0 +1,40 @@
+local Input = io.open("input.txt", "r")
+
+local line = Input:read("*l")
+local LineN = 1
+local ColumnTargetSpace = {}
+for i = 1, #line do
+	ColumnTargetSpace[i] = 1
+end
+local NewRockMap = {}
+while line do
+	local RockRow = {}
+	NewRockMap[LineN] = RockRow
+	for i = 1, #line do
+		local char = line:sub(i, i)
+		if char == "O" then
+			RockRow[i] = "."
+			NewRockMap[ColumnTargetSpace[i]][i] = "O"
+			ColumnTargetSpace[i] = ColumnTargetSpace[i] + 1
+		elseif char == "#" then
+			ColumnTargetSpace[i] = LineN + 1
+			RockRow[i] = "#"
+		else
+			RockRow[i] = "."
+		end
+	end
+	line = Input:read("*l")
+	LineN = LineN + 1
+end
+print("Rolled all the rocks north")
+
+local Total = 0
+for i,row in next,NewRockMap do
+	for j = 1, #row do
+		if row[j] == "O" then
+			Total = Total + (#NewRockMap-i+1)
+		end
+	end
+end
+
+print("Total load:", Total)
