bb5fa11451c11d559cec297ce988b069a57c871a
9382 <omnomaidan@gmail.com>
2023-12-15 07:43:34 +0000

    very very minor improvement to speed

--- 15/code.py
+++ 15/code.py
@@ -3,8 +3,8 @@ Input = open("input.txt", "r").read()
 def PerformHashing(string):
 	value = 0
 	for char in string:
-		value = ((value + ord(char)) * 17) % 256
-	return value
+		value = ((value + ord(char)) * 17)
+	return value % 256
 
 Total = 0
 for string in Input.replace("\n", "").split(","):
